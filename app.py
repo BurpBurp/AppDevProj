d28d1afb19d1f3ab35388cf56daa082fdfcdb6c5
BurpBurp <114080277+BurpBurp@users.noreply.github.com>
2022-10-18 21:31:05 +0800

    Testing

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-db = SQLAlchemy(app)
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow())
-
-    def __repr__(self):
-        return f"<Task {self.id}>"
-
 
 @app.route('/')
 def index():
@@ -22,11 +9,11 @@ def index():
 
 @app.route("/login", methods = ["GET","POST"])
 def login():
-    return render_template()
+    return render_template("login.html")
 
 @app.route("/usr")
 def user(usr):
-    return render_template(f"<h1>{usr}</h1>")
+    return f"<h1>{usr}</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
