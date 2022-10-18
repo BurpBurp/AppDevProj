50fd15836fcaa79bada7b44844740a5987a45e7d
BurpBurp <114080277+BurpBurp@users.noreply.github.com>
2022-10-18 20:26:24 +0800

    Testing

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("home.html", title="Homepage")
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
