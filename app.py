15ce15944a4a97c60630c1e86ec39ab276761bdc
BurpBurp <114080277+BurpBurp@users.noreply.github.com>
2022-10-19 22:28:10 +0800

    Moved all routes to routes directory Added compatibility for DB from blueprints Cleaned up app.py

--- app.py
+++ app.py
@@ -1,91 +1,36 @@
-import sqlalchemy
-from flask import Flask, render_template, redirect, url_for, session, request, flash
+from flask import Flask, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import exc, select, Column, text, and_, or_
-from flask_wtf.csrf import CSRFProtect
-import User
+from database import db
+import os
+import routes.test as test
+import routes.signup as signup
+import routes.index as index
+from UserDBModel import User
+from flask_wtf import CSRFProtect
 
 
-
-# Flask and DB Init
-app = Flask(__name__)
-csrf = CSRFProtect(app)
-
-app.config['SECRET_KEY'] = 'mysecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
-app.config['SESSION_TYPE'] = 'sqlalchemy'
-
-db = SQLAlchemy(app)
-User.db.init_app(app)
-db.init_app(app)
-
-
-app.config['SESSION_SQLALCHEMY'] = db
-# Session Init
-Session(app)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///master.db"
+    app.config['SECRET_KEY'] = 'mysecret'
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    app.config['SESSION_SQLALCHEMY'] = db
+    csrf = CSRFProtect(app)
+    db.init_app(app)
+    app.register_blueprint(test.blueprint)
+    app.register_blueprint(signup.blueprint)
+    app.register_blueprint(index.blueprint)
+    Session(app)
+    return app
 
 
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        password = request.form["password"]
-        newUser = User.User(username=name,email=email,password=password)
-        existingUser = db.session.execute(select(User.User).where(or_(User.User.username == name, User.User.email == email))).scalar()
-        if not existingUser:
-            try:
-                db.session.add(newUser)
-                db.session.commit()
-                session["username"] = name
-                db.session.close()
-            except exc.IntegrityError:
-                flash("User.User already exists")
-                return render_template("signup.html")
-        else:
-            flash("User.User already exists")
-            return render_template("signup.html")
-        return redirect(url_for("index"))
-    else:
-        if "username" in session:
-            print(f"Session Cached, {session['username']}")
-            return redirect(url_for("index"))
-        return render_template("signup.html")
-
-@app.route("/signout", methods=["GET","POST"])
-def signout():
-    session.pop("username",None)
-    return redirect(url_for("index"))
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        name = request.form["name"]
-        password = request.form["password"]
-        existingUser = db.session.execute(select(User.User).where(and_(User.User.username == name, User.User.password == password))).scalar()
-        if existingUser:
-            session["username"] = existingUser.username
-            flash("Logged in Successfully")
-            return redirect(url_for("index"))
-        else:
-            flash("Username/Password Wrong")
-            return render_template("login.html")
-    else:
-        if "username" in session:
-            print(f"Session Cached, {session['username']}")
-            flash("Logged in Successfully")
-            return redirect(url_for("index"))
-        return render_template("login.html")
-
-
-
-if __name__ == '__main__':
+def setup_database(app: Flask):
     with app.app_context():
         db.create_all()
-        User.db.create_all()
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app: Flask = create_app()
+    if not os.path.isfile('instance/master.db'):
+        setup_database(app)
+    app.run()
