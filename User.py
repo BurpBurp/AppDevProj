1c742ff7d1d3079b399af2581e3f93666118d580
BurpBurp <114080277+BurpBurp@users.noreply.github.com>
2022-10-19 20:25:12 +0800

    Added User Login     - login.html Moved User DB Model to User.py Fixed requirements.txt Moved Signup -> signup.html Added Flash Fixed Integrity Rollback

---
+++ User.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String, default="user")
+    cart_id = db.Column(db.String,nullable=True)
--- app.py
+++ app.py
@@ -1,73 +1,91 @@
-from flask import Flask, render_template, redirect, url_for, session, request
+import sqlalchemy
+from flask import Flask, render_template, redirect, url_for, session, request, flash
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import exc
+from sqlalchemy import exc, select, Column, text, and_, or_
+from flask_wtf.csrf import CSRFProtect
+import User
+
+
 
 # Flask and DB Init
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 app.config['SECRET_KEY'] = 'mysecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
 app.config['SESSION_TYPE'] = 'sqlalchemy'
 
 db = SQLAlchemy(app)
+User.db.init_app(app)
 db.init_app(app)
+
 
 app.config['SESSION_SQLALCHEMY'] = db
 # Session Init
 Session(app)
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    role = db.Column(db.String, default="user")
-
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
-
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
     if request.method == "POST":
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
-        newUser = User(name, email, password)
-        try:
-            db.session.add(newUser)
+        newUser = User.User(username=name,email=email,password=password)
+        existingUser = db.session.execute(select(User.User).where(or_(User.User.username == name, User.User.email == email))).scalar()
+        if not existingUser:
             try:
+                db.session.add(newUser)
                 db.session.commit()
-            except exc.PendingRollbackError:
-                print("Rollback")
-                db.session.rollback()
-                raise
-            finally:
+                session["username"] = name
                 db.session.close()
-        except exc.IntegrityError:
-            print("Duplicate")
-            return "ERROR USER/EMAIL ALREADY EXISTS"
-        print(db.session.execute(db.select(User).where(User.role == "user")).scalar().role)
-        session["id"] = newUser.id
+            except exc.IntegrityError:
+                flash("User.User already exists")
+                return render_template("signup.html")
+        else:
+            flash("User.User already exists")
+            return render_template("signup.html")
         return redirect(url_for("index"))
     else:
         if "username" in session:
-            print(f"Session Cached, {session['id']}")
+            print(f"Session Cached, {session['username']}")
+            return redirect(url_for("index"))
+        return render_template("signup.html")
+
+@app.route("/signout", methods=["GET","POST"])
+def signout():
+    session.pop("username",None)
+    return redirect(url_for("index"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        existingUser = db.session.execute(select(User.User).where(and_(User.User.username == name, User.User.password == password))).scalar()
+        if existingUser:
+            session["username"] = existingUser.username
+            flash("Logged in Successfully")
+            return redirect(url_for("index"))
+        else:
+            flash("Username/Password Wrong")
+            return render_template("login.html")
+    else:
+        if "username" in session:
+            print(f"Session Cached, {session['username']}")
+            flash("Logged in Successfully")
             return redirect(url_for("index"))
         return render_template("login.html")
+
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        User.db.create_all()
     app.run(debug=True)
