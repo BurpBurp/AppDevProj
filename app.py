46da135d5d3ccbc2cc4bc143d6e48211f1873d5e
Darwin <kinglol1409@gmail.com>
2022-10-19 11:26:06 +0800

    Added Sessions,

--- app.py
+++ app.py
@@ -1,49 +1,64 @@
 from flask import Flask, render_template, redirect, url_for, session, request
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 
-db = SQLAlchemy()
+# Flask and DB Init
 app = Flask(__name__)
+
+app.secret_key = 'the random string'
+app.config['SECRET_KEY'] = 'mysecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
+app.config['SESSION_TYPE'] = 'sqlalchemy'
+
+db = SQLAlchemy(app)
 db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String,unique = True, nullable = False)
-    email = db.Column(db.String,unique = True, nullable = False)
-    password = db.Column(db.String,nullable = False)
-    role = db.Column(db.String,default="user")
+app.config['SESSION_SQLALCHEMY'] = db
+# Session Init
+Session(app)
 
-    def __init__(self,username,email,password):
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String, default="user")
+
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods = ["GET","POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
-        existingUserByName : User = db.session.execute(db.select(User).where(User.username == name)).scalar()
-        existingUserByEmail : User = db.session.execute(db.select(User).where(User.email == email)).scalar()
-        newUser = User(name,email,password)
+        newUser = User(name, email, password)
         try:
             db.session.add(newUser)
             db.session.commit()
         except exc.IntegrityError:
             return "ERROR USER/EMAIL ALREADY EXISTS"
         print(db.session.execute(db.select(User).where(User.role == "user")).scalar().role)
+        session["username"] = name
         return redirect(url_for("index"))
-    return render_template("login.html")
+    else:
+        if "username" in session:
+            print("Session Cached")
+            return redirect(url_for("index"))
+        return render_template("login.html")
 
-@app.route("/usr")
-def user(usr):
-    return f"<h1>{usr}</h1>"
 
 if __name__ == '__main__':
     with app.app_context():
