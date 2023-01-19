1ef5d0943e90afc73c75986bbcdb249d9c978e89
Jeron <jeronwan8@gmail.com>
2023-01-19 09:58:05 +0800

    MultipleFileFields

--- forms/AddItemForm.py
+++ forms/AddItemForm.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField, SelectField, IntegerField, DecimalField, FileField
+from wtforms import StringField, PasswordField, EmailField, SubmitField, SelectField, IntegerField, DecimalField, MultipleFileField
 from wtforms.validators import DataRequired, Email, EqualTo, NumberRange
 from flask_wtf.file import FileRequired, FileAllowed
 
@@ -7,7 +7,7 @@ class AddItemForm(FlaskForm):
     name = StringField("Item Name",validators=[DataRequired()])
     quantity = IntegerField("Quantity",validators=[DataRequired(), NumberRange(min=0, message="Quantity cannot be less than 0")])
     price = DecimalField("Price of Item",places=2, validators=[DataRequired(), NumberRange(min=0, message="Rich man FOC?")])
-    image = FileField("Picture of Item")
+    image = MultipleFileField("Picture of Item", validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
     category = StringField("Type of Item")
     submit = SubmitField("Add Item")
 
@@ -15,6 +15,6 @@ class UpdateItemForm(FlaskForm):
     name = StringField("Item Name",validators=[DataRequired()])
     quantity = IntegerField("Quantity",validators=[DataRequired(), NumberRange(min=0, message="Quantity cannot be less than 0")])
     price = DecimalField("Price of Item", validators=[DataRequired(), NumberRange(min=0, message="Rich man FOC?")])
-    image = FileField("Picture of Item")
+    image = MultipleFileField("Picture of Item", validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
     category = StringField("Type of Item")
     submit = SubmitField("Update Item")
