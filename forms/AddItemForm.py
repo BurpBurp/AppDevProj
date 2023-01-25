2110941169ebce9f78233e00b6cac15d6ec6c9fb
Jeron <jeronwan8@gmail.com>
2023-01-26 01:50:27 +0800

    Added Modals and validator message

--- forms/AddItemForm.py
+++ forms/AddItemForm.py
@@ -6,10 +6,10 @@ from forms.CustomValidators import MultiFileAllowed
 from flask_wtf.file import FileRequired, FileAllowed
 
 class AddItemForm(FlaskForm):
-    name = StringField("Item Name",validators=[DataRequired()])
-    quantity = IntegerField("Quantity",validators=[DataRequired(), NumberRange(min=0, message="Quantity cannot be less than 0")])
-    price = DecimalField("Price of Item",places=2, validators=[DataRequired(), NumberRange(min=0, message="Rich man FOC?")])
-    image = MultipleFileField("Picture of Item", validators=[MultiFileAllowed(["jpg","png","jpeg"],message=".jpg, .png or .jpeg File Required")])
+    name = StringField("Item Name",validators=[DataRequired("Please enter name of item")])
+    quantity = IntegerField("Quantity",validators=[DataRequired("Please enter number of items to add"), NumberRange(min=0, message="Quantity cannot be less than 0")])
+    price = DecimalField("Price of Item",places=2, validators=[DataRequired("Please enter price of item"), NumberRange(min=0, message="Rich man FOC?")])
+    image = MultipleFileField("Picture of Item", validators=[MultiFileAllowed(["jpg", "png", "jpeg"],message=".jpg, .png or .jpeg File Required")])
     category = StringField("Type of Item")
     submit = SubmitField("Add Item")
 
