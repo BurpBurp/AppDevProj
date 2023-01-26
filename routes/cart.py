76ffda69b412189b7ebc807ce6d47ff02fcbb5e6
padricgoh <padricgoh@gmail.com>
2023-01-26 16:12:55 +0800

    work in progress

--- routes/cart.py
+++ routes/cart.py
@@ -2,6 +2,7 @@ from flask import Blueprint, session, redirect, url_for, request, render_templat
 import flask_login
 import helper_functions
 from forms.AddToCartForm import AddToCartForm
+from forms.UpdateQtyForm import UpdateQtyForm
 from database_models.UserDBModel import *
 from database_models.CartDBModel import *
 
@@ -13,7 +14,7 @@ def read_cart():
     cart = flask_login.current_user.cart.cart_items
     return render_template("Checkout/readcart.html",cart=cart)
 
-@blueprint.route("/cart/add_to_cart/<id>", methods=["GET","POST"])
+@blueprint.route("/cart/add_to_cart/<id>/", methods=["GET","POST"])
 @flask_login.login_required
 def add_to_cart(id):
     form = AddToCartForm()
@@ -34,6 +35,21 @@ def add_to_cart(id):
         return redirect(url_for("cart.read_cart"))
     return render_template("Checkout/addtocart.html",item=item,form=form)
     pass
+
+
+@blueprint.route("/cart/update/<id>/", methods=["GET","POST"])
+@flask_login.login_required
+def update_qty(id):
+    form = UpdateQtyForm()
+    item = Cart_Item.query.filter_by(id=id,cart=flask_login.current_user.cart).first()
+    print(request.method)
+    if request.method == "POST":
+        item.quantity = int(form.quantity.data)
+        db.session.commit()
+        return redirect(url_for("cart.read_cart"))
+    helper_functions.flash_error("Update Failed")
+    return redirect(url_for("cart.read_cart"))
+
 
 @blueprint.route("/cart/delete_item/<id>")
 @flask_login.login_required
