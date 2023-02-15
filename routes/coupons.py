4d105b138ed0bd42340f146535e6c172924aa2ab
BurpBurp <114080277+BurpBurp@users.noreply.github.com>
2023-02-16 01:35:57 +0800

    Changed Error Messages Fixed Create Code cannot be the same error Fixed Edit Code cannot be the same error

--- routes/coupons.py
+++ routes/coupons.py
@@ -9,7 +9,8 @@ import helper_functions
 
 blueprint = Blueprint("coupon", __name__, template_folder="templates")
 
-@blueprint.route("/coupon/add", methods=["POST","GET"])
+
+@blueprint.route("/coupon/add", methods=["POST", "GET"])
 @flask_login.login_required
 @helper_functions.admin_required
 def add_coupon():
@@ -24,20 +25,28 @@ def add_coupon():
                     form.coupon_discount.errors.append("Value must be less than 100")
                     return render_template("coupon/add_coupon.html", form=form)
 
-                coupon = stripe.Coupon.create(percent_off=form.coupon_discount.data,duration="forever",name=form.name.data)
+                coupon = stripe.Coupon.create(percent_off=form.coupon_discount.data, duration="forever",
+                                              name=form.name.data)
             else:
-                coupon = stripe.Coupon.create(amount_off=form.coupon_discount.data * 100,duration="forever",name=form.name.data,currency="SGD")
+                coupon = stripe.Coupon.create(amount_off=form.coupon_discount.data * 100, duration="forever",
+                                              name=form.name.data, currency="SGD")
             codes_in_use = []
 
+            all_codes = [str(code).upper() for code in form.codes.data]
+            if len(set(all_codes)) != len(all_codes):
+                helper_functions.flash_error(f"Error! Codes Cannot Be the same")
+                stripe.Coupon.delete(coupon["id"])
+                return render_template("coupon/add_coupon.html", form=form)
+
             for code in form.codes.data:
-                retrived_code = stripe.PromotionCode.list(code=code,active=True)
+                retrived_code = stripe.PromotionCode.list(code=code, active=True)
                 print(retrived_code)
                 if len(retrived_code["data"]) > 0:
                     codes_in_use.append(code)
 
             if len(codes_in_use) > 0:
                 for code in codes_in_use:
-                    helper_functions.flash_error(f"Code {code} in use")
+                    helper_functions.flash_error(f"Error! Code {code} in use")
                 stripe.Coupon.delete(coupon["id"])
                 return render_template("coupon/add_coupon.html", form=form)
             for code in form.codes.data:
@@ -52,7 +61,8 @@ def add_coupon():
 def view_coupons():
     coupons = stripe.Coupon.list()["data"]
     print(coupons)
-    return render_template("coupon/view_coupons.html", couponList= coupons)
+    return render_template("coupon/view_coupons.html", couponList=coupons)
+
 
 @blueprint.route("/coupon/delete/<id>/", methods=["GET"])
 @blueprint.route("/coupon/delete/<id>", methods=["GET"])
@@ -62,14 +72,14 @@ def delete_coupons(id):
     try:
         stripe.Coupon.retrieve(id)
         stripe.Coupon.delete(id)
-        helper_functions.flash_success("Deleted Coupon")
+        helper_functions.flash_success("Success! Deleted Coupon")
     except stripe.error.StripeError:
-        helper_functions.flash_error("Coupon Not FOund")
+        helper_functions.flash_error("Error! Coupon Not FOund")
     return redirect(url_for("coupon.view_coupons"))
 
 
-@blueprint.route("/coupon/edit/<id>/", methods=["GET","POST"])
-@blueprint.route("/coupon/edit/<id>", methods=["GET","POST"])
+@blueprint.route("/coupon/edit/<id>/", methods=["GET", "POST"])
+@blueprint.route("/coupon/edit/<id>", methods=["GET", "POST"])
 @flask_login.login_required
 @helper_functions.admin_required
 def edit_coupon(id):
@@ -86,7 +96,7 @@ def edit_coupon(id):
     elif request.method == "POST":
         if form.validate_on_submit():
             print("VALIDATED")
-            stripe.Coupon.modify(id,name=form.name.data)
+            stripe.Coupon.modify(id, name=form.name.data)
             print("NAMECHANGED")
             print(form.codes.data)
             if len(form.codes.data) > 0:
@@ -94,14 +104,19 @@ def edit_coupon(id):
                 codes_in_use = []
 
                 for code in form.codes.data:
-                    retrived_code = stripe.PromotionCode.list(code=code,active=True)
+                    retrived_code = stripe.PromotionCode.list(code=code, active=True)
                     print(retrived_code)
                     if len(retrived_code["data"]) > 0:
                         codes_in_use.append(code)
 
+                all_codes = [str(code).upper() for code in form.codes.data]
+                if len(set(all_codes)) != len(all_codes):
+                    helper_functions.flash_error(f"Error! Codes Cannot Be the same")
+                    return render_template("coupon/edit_coupon.html", coupon=coupon, codes=codes["data"], form=form)
+
                 if len(codes_in_use) > 0:
                     for code in codes_in_use:
-                        helper_functions.flash_error(f"Code {code} in use")
+                        helper_functions.flash_error(f"Error! Code {code} in use")
                     return render_template("coupon/edit_coupon.html", coupon=coupon, codes=codes["data"], form=form)
 
                 for code in form.codes.data:
@@ -117,8 +132,8 @@ def activate_code():
     try:
         if id := request.form.get("id"):
             stripe.PromotionCode.modify(
-              id,
-              active=True
+                id,
+                active=True
             )
             return jsonify(success=1)
     except stripe.error.StripeError:
@@ -131,8 +146,8 @@ def deactivate_code():
     try:
         if id := request.form.get("id"):
             stripe.PromotionCode.modify(
-              id,
-              active=False
+                id,
+                active=False
             )
             return jsonify(success=1)
     except stripe.error.StripeError:
